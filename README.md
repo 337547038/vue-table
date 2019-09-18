f33e64d769ddaba9c14026b31621f295b98c96f1
337547038 <337547038@qq.com>
2019-09-18 15:46:12 +0800

    add

---
+++ README.md
@@ -0,0 +1,43 @@
+# vue table
+
+## 示例演示地址
+https://337547038.github.io/akvue/#/table
+
+### - √ 可固定表头
+### - √ 可固定列
+### - √ 可添加序号
+### - √ 支持勾选
+### - √ 可排序
+### - √ 可固定列宽
+### - √ 可添加扩展列
+### - √ 可拖动改变列宽
+### - √ 可合并行或列
+### - √ 支持多级表头
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>vue table</h1>
+    <p>more demo <a href="https://337547038.github.io/akvue/#/table">https://337547038.github.io/akvue/#/table</a> </p>
+    <Table :data="data">
+      <TableColumn label="date" prop="date"></TableColumn>
+      <TableColumn label="name" prop="name"></TableColumn>
+      <TableColumn label="tel" prop="tel"></TableColumn>
+      <TableColumn label="address" prop="address"></TableColumn>
+      <TableColumn label="control">
+        <template slot-scope="scope">
+          del
+        </template>
+      </TableColumn>
+    </Table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import '../assets/_table.scss'
+import {Table, TableColumn} from '@/components/table'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      data: [
+        {
+          date: '2019-01-01',
+          name: 'zhang san',
+          tel: '138000138',
+          address:'address'
+        },
+        {
+          date: '2019-01-02',
+          name: 'zhangA',
+          tel: '138000138',
+          address:'address'
+        }
+      ]
+    }
+  },
+  components: {
+    Table, TableColumn
+  }
+}
+</script>
